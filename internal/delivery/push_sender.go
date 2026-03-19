70a5759d90cce2585a86cbc84de97f1a063e484e
Supriyo <supriyomalstartup@gmail.com>
2026-03-19 17:46:53 +0530

    fix: gofmt and minor syntax cleanup in push sender

--- internal/delivery/push_sender.go
+++ internal/delivery/push_sender.go
@@ -1,18 +1,19 @@
 package delivery
 
 import (
-	"context",
-	"notification-service/internal/model"
+	"context"
 
 	firebase "firebase.google.com/go"
 	"firebase.google.com/go/messaging"
+
+	"notification-service/internal/model"
 )
 
 type PushSender struct {
 	client *messaging.Client
 }
 
-func NewPushSender(app *firebase.App) (*PushSender, error){
+func NewPushSender(app *firebase.App) (*PushSender, error) {
 	client, err := app.Messaging(context.Background())
 	if err != nil {
 		return nil, err
@@ -24,11 +25,11 @@ func (s *PushSender) Send(n *model.Notification) error {
 	msg := &messaging.Message{
 		Notification: &messaging.Notification{
 			Title: n.Title,
-			Body: n.Body,
+			Body:  n.Body,
 		},
 		Token: "DEVICE_FCM_TOCKEN", // todo -> later from db
 	}
-	
-	_, err := s.client.Send(context.Background(),msg)
+
+	_, err := s.client.Send(context.Background(), msg)
 	return err
 }
